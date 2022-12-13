0e5747e1fa973d9d06b2e0346af8ab3ff3b4c30e
abhirajsinh <raolabhirajsinh@gmail.com>
2022-12-13 10:45:19 +0530

    array

---
+++ c/Class work/arrayacceptdisplay.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+	int arr1[5];
+	int i;
+	
+	printf("\nEnter Array 1 Elements : \n");
+
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",array[i]);
+	}
+	printf("\n\nArray 1 Elements are : \n\n");
+	for(i=0;i,5;i++)
+	{
+		printf("%d\t",array[i]);
+	}
+}
